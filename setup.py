ce011e1c4355cc83f733f5debaeabbf6a4405e7a
hauptmech <hauptmech@gmail.com>
2023-01-08 09:43:59 +0800

    Change indent to spaces, change versioning tool

--- setup.py
+++ setup.py
@@ -13,21 +13,22 @@ ext_modules = [
     Pybind11Extension(
         MODULE_NAME,
         sorted(binding_source_files),  # Sort source files for reproducibility
-		include_dirs=["innfos-cpp-sdk/sdk/include"],
-		libraries=["ActuatorController"],
-		library_dirs=["innfos-cpp-sdk/sdk/lib/linux_x86_64/"],
-		extra_link_args=["-Wl,-rpath,$ORIGIN"], # Needed so libActuatorController.so can be found by clover_ActuatorController
+        include_dirs=["innfos-cpp-sdk/sdk/include"],
+        libraries=["ActuatorController"],
+        library_dirs=["innfos-cpp-sdk/sdk/lib/linux_x86_64/"],
+        extra_link_args=["-Wl,-rpath,$ORIGIN"], # Needed so libActuatorController.so can be found by clover_ActuatorController
     ),
 ]
 
 setup(
     name='clover_innfos_python',
-	packages=['clover_innfos_python'],
-	setup_requires = ['pybind11','setuptools-git-versioning<2'],
+    packages=['clover_innfos_python'],
+    setup_requires = ['pybind11','setuptools-git-versioning<2','setuptools_scm'],
     install_requires = ["matplotlib", "scipy", "pandas", "numpy"],
     provides = ["clover_innfos_python"],
     ext_modules=ext_modules,
-	data_files=[('',['innfos-cpp-sdk/sdk/lib/linux_x86_64/libActuatorController.so'])],
-	setuptools_git_versioning={"enabled": True,},
+    data_files=[('',['innfos-cpp-sdk/sdk/lib/linux_x86_64/libActuatorController.so'])],
+    #setuptools_git_versioning={"enabled": True,},
+    use_scm_version=True,
 )
 
